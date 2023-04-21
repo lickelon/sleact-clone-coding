9e0aaeea1037d9f0ebff3ff3f36f0bcdb58e4d99
lickelon <jindoman0@naver.com>
2023-04-22 03:13:43 +0900

    섹션1

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import loadable from '@loadable/component';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
-  return <div>초기 세팅입니다.</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
